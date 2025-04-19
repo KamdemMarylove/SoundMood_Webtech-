cc002f214b83c5bf81faa1f51137f7ba2c6377f8
KamdemMarylove <mrslover8000@yahoo.com>
2025-04-19 15:20:13 +0200

    Initial commit: full project uploaded

---
+++ src/main/java/io/WebTech/SoundMood/SoundMoodApplication.java
@@ -0,0 +1,13 @@
+package io.WebTech.SoundMood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundMoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundMoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/WebTech/SoundMood/SoundMoodApplicationTests.java
@@ -0,0 +1,13 @@
+package io.WebTech.SoundMood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundMoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
