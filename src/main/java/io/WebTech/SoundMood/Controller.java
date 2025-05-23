934617ee1cd4bcf27879ea02c696a7f3e055453b
aliya <aliyaguelaktuerk@gmail.com>
2025-05-23 22:35:39 +0200

    controller connection

--- src/main/java/io/WebTech/SoundMood/Controller.java
+++ src/main/java/io/WebTech/SoundMood/Controller.java
@@ -1,18 +1,19 @@
 package io.WebTech.SoundMood;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "https://frontend-soundmood.onrender.com")
 @RestController
 public class Controller {
     @GetMapping("/mood")
     public List<SongVorschlag> getSongs() {
         return List.of(
                 new SongVorschlag("Pona nini", "Tiakola", "happy")
-    );
+        );
     }
-
-
 }
