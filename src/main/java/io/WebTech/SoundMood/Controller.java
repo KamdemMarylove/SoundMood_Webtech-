f332942ad1611044be932f1088ead43503cc27d3
aliya <aliyaguelaktuerk@gmail.com>
2025-06-19 18:17:28 +0200

    frontend backend kommunikation

--- src/main/java/io/WebTech/SoundMood/Controller.java
+++ src/main/java/io/WebTech/SoundMood/Controller.java
@@ -2,14 +2,18 @@ package io.WebTech.SoundMood;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
-@CrossOrigin(origins = "https://frontend-soundmood.onrender.com")
+@CrossOrigin(
+        origins = "https://frontend-soundmood.onrender.com",
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS}
+)
 @RestController
 public class Controller {
+
     @GetMapping("/mood")
     public List<SongVorschlag> getSongs() {
         return List.of(
